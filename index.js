e9d82956aed38bd2f068f28b1d30e14a94664918
junjun <xingfu963@users.noreply.github.com>
2019-11-26 10:50:18 +0800

    更新项目的使用情况

---
+++ index.js
@@ -0,0 +1,44 @@
+function myFunction()
+{
+
+    txt1="what a  very";
+    txt2="nice day";
+    txt3=txt1+txt2;
+    document.getElementById(_"demo").innerHTML=text3;
+}
+function myFunction(){
+
+
+    var  str="visit Runoob!";
+    var n=str.search("Runoob");
+    document.getElementById("demo").innerHTML=n;
+
+}
+document,getELementById("myBtn").addEventListener("click");
+alert("hello world");
+
+function myFunction(x,y)
+{
+    if(y===undefined){
+        y=0;
+
+    }
+    return  x*y;
+
+}
+document.getElementById("demo").innerHTML=myFunction 
+
+var xiaoming={
+     name:'小明'，
+     age:14,
+     gende:true,
+     height:1.65,
+     grade:null,
+     "middle-school":''.anchor//.
+
+}
+var s=JSON.stringify(xiaoming);
+console.log(s);
+JSON.stringify(xiaoming,null,'')
+
+
